62df59cbb1a26a7c5097f699a428732f723c3b27
carlaraya <carl.araya256@gmail.com>
2018-07-24 00:56:03 +0800

    sleep and clear

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
+import random, time
 
 with open('art.txt') as art_file:
     logos = art_file.read().split('------')
@@ -6,6 +6,7 @@ logos = dict([logo.strip().split('\n', 1) for logo in logos])
 
 teams = []
 while True:
+    print(chr(27) + "[2J")
     name = input('Enter your name: ')
     if name.lower() == 'exit':
         break
@@ -17,6 +18,7 @@ while True:
     print(logos[assigned_team])
     with open('groupings.txt', 'a') as groupings:
         groupings.write('%s : %s\n' % (name, assigned_team))
+    time.sleep(3)
 
 with open('groupings.txt', 'r') as groupings, open('groupings_sorted.txt', 'w') as groupings_sorted:
     gs = groupings.read().strip().split('\n')
